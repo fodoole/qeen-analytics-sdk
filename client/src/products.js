ec3c9e75a166ed58d1b2d2eae20df5baf1942059
omartalafhah <omar.talafhah@fodoole.com>
2024-07-20 16:03:27 +0300

    dev playground

--- client/src/products.js
+++ client/src/products.js
@@ -1,24 +1,24 @@
 const products = [
   {
     id: 1,
-    title: "Craig",
+    title: "Product 1",
     price: "80",
-    image: "https://brawlhalla.wiki.gg/images/c/c7/Bot_Craig.png",
-    description: "Lightly used. Cash only. No cops. Seller not responsible for dropped respawns.",
+    image: "https://picsum.photos/id/10/1024/1024",
+    description: "Product description.",
   },
   {
     id: 2,
-    title: "Dander",
+    title: "Product 2",
     price: "30",
-    image: "https://brawlhalla.wiki.gg/images/1/1e/Avatar_Dander.png",
-    description: "I can has tibia?",
+    image: "https://picsum.photos/id/39/1024/1024",
+    description: "Product description.",
   },
   {
     id: 3,
-    title: "Squidward's Clarinet",
+    title: "Product 3",
     price: "60",
-    image: "https://brawlhalla.wiki.gg/images/0/00/Squidward%27s_Clarinet.png",
-    description: "Not a toilet plunger.",
+    image: "https://picsum.photos/id/11/1024/1024",
+    description: "Product description.",
   }
 ];
 
