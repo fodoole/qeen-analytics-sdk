0e78b1ae92fe916962d926e3266c2ba4c2821b15
GhSewar <sewar@fodoole.com>
2024-07-30 15:49:31 +0300

    open home page on '/' route

--- Demo/frontend/src/App.jsx
+++ Demo/frontend/src/App.jsx
@@ -12,6 +12,7 @@ function App() {
     <>
       <Navbar />
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="home" element={<Home />} />
         <Route path="/products/:id" element={<Item />} />
         <Route path="/categoreis/:id" element={<Category />} />
