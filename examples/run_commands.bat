59c39f881f7541c8d2f75a2248431de77e4e061f
GhSewar <sewar@fodoole.com>
2024-08-28 13:49:13 +0300

    rename endpoint to config_server

--- examples/run_commands.bat
+++ examples/run_commands.bat
@@ -2,4 +2,4 @@
 
 start "" /D ".\react\frontend" cmd /c "npm run start"
 start "" /D ".\react\backend" cmd /c "npm run dev"
-start "" /D ".\endpoint" cmd /c "node index.js"
+start "" /D ".\configServer" cmd /c "node index.js"
